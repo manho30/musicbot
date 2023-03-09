ca1a56825e2261a41a12f151bdb204e8ee3d3849
manho30 <manhochan30@gmail.com>
2023-03-09 22:23:24 +0800

    first commit

--- api/api.py
+++ api/api.py
@@ -9,12 +9,10 @@
 """
 
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 
 from bot.util import bot
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/', methods=['GET'])
 def index():
